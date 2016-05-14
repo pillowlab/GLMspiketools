5ce589973c4b2539e9104f8bef52f6720929e648
Jonathan Pillow <pillow@mail.utexas.edu>
2016-05-14 02:42:28 -0400

    fixed up MAP inference for bilinear GLM (coord ascent)

--- glmtools_fitting/MAPfit_GLMbi_coordascent.m
+++ glmtools_fitting/MAPfit_GLMbi_coordascent.m
@@ -1,5 +1,5 @@
-function [gg,fval,H] = MAPfit_GLMbi_coordascent(gg,Stim,Ct,Cx,maxiter,ftol,optimArgs)
-%  [gg,fval,H] = MAPfit_GLMbi_coordascent(gg,Stim,maxiter,ftol,optimArgs);
+function [gg,neglogli,H,neglogp] = MAPfit_GLMbi_coordascent(gg,Stim,Cx,Ct,maxiter,ftol,optimArgs)
+%  [gg,neglogli,H,neglogp] = MAPfit_GLMbi_coordascent(gg,Stim,maxiter,ftol,optimArgs);
 % 
 %  Computes the MAP estimate for GLM params, with gradient and hessians, via
 %  coordinate ascent, for bilinear (low rank) parametrization of space-time filter.
@@ -7,8 +7,8 @@ function [gg,fval,H] = MAPfit_GLMbi_coordascent(gg,Stim,Ct,Cx,maxiter,ftol,optim
 %  Inputs: 
 %     gg = param struct
 %     Stim = stimulus
-%       Ct = inverse covariance for temporal params
 %       Cx = inverse covariance for spatial params
+%       Ct = inverse covariance for temporal params
 %     maxiter = maximum number of coordinate ascent steps 
 %     ftol = tolerance for stopping based on function improvement 
 %     optimArgs = cell array of optimization params (optional)
@@ -51,17 +51,23 @@ xStim = zeros(nt,krank*nkx); % initialize spatial stimulus
 
 % compute initial log-likelihood
 neglogli0 = neglogli_GLM(gg,Stim); % Compute logli of initial params
-dlogli = inf;  % initialize change in logli
+dlogp = inf;  % initialize change in log-posterior
 jjiter = 0;  % initialize counter
 
-while (jjiter<maxiter) && dlogli>ftol
+% compute initial penalties (negative log prior)
+nlpx = .5*gg.kx(:)'*Cx*gg.kx(:)
+nlpt = .5*gg.kt(:)'*Ct*gg.kt(:)
+neglogp0 = neglogli0+nlpx+nlpt;
+
+while (jjiter<maxiter) && dlogp>ftol
     
     % ---- Update temporal params -----
     fprintf('Iter #%d: Updating temporal params\n', jjiter);
     tStim = Stim*reshape(ggx.k',[],gg.krank);
     ggt.dc = ggx.dc;  % update dc param
     [ggt,tneglogli] = MAPfit_GLM(ggt,tStim,Ct,optimArgs);
-    fprintf('dlogli = %.4f\n\n', neglogli0-tneglogli);
+    nlpt = .5*ggt.kt(:)'*Ct*ggt.kt(:);
+    fprintf('dlogp = %.4f\n\n', neglogp0-(tneglogli+nlpt+nlpx));
     
     % Convolve stimulus with temporal filters
     for irank = 1:krank
@@ -74,16 +80,18 @@ while (jjiter<maxiter) && dlogli>ftol
     fprintf('Iter #%d: Updating spatial params\n', jjiter);
     ggx.dc = ggt.dc; % update dc param
     [ggx,xneglogli] = MAPfit_GLM(ggx,xStim,Cx,optimArgs);
-    fprintf('dlogli = %.4f\n\n', neglogli0-xneglogli);
-    
+    nlpx = .5*ggx.k(:)'*Cx*ggx.k(:);
+    neglogp = xneglogli+nlpt+nlpx;
+    fprintf('dlogp = %.4f\n\n', neglogp0-neglogp);
+
     % Update iters
     jjiter = jjiter+1;  % counter
-    dlogli = neglogli0-xneglogli; % change in log-likelihood
-    neglogli0 = xneglogli;
+    dlogp = neglogp0-neglogp; % change in log-likelihood
+    neglogp0 = neglogp;
     
 end
 
-fprintf('\nFinished coordinate ascent: %d iterations (dlogli=%.6f)\n',jjiter,dlogli);
+fprintf('\nFinished coordinate ascent: %d iterations (dlogp=%.6f)\n',jjiter,dlogp);
 
 % Compute conditional Hessians, if desired
 if nargout > 2
@@ -92,6 +100,7 @@ if nargout > 2
     tStim = Stim*(ggx.k');
     ggt.dc = ggx.dc;  % update dc param
     [ggt,~,Ht] = MAPfit_GLM(ggt,tStim,Ct,optimArgs);
+    nlpt = .5*ggt.kt(:)'*Ct*ggt.kt(:);
 
     % Compute Hessian for space components
     for irank = 1:krank
@@ -100,8 +109,9 @@ if nargout > 2
         end
     end    
     ggx.dc = ggt.dc; % update dc param
-    [ggx,neglogli0,Hx] = MAPfit_GLM(ggx,xStim,Cx,optimArgs);
-    
+    [ggx,xneglogli,Hx] = MAPfit_GLM(ggx,xStim,Cx,optimArgs);
+    nlpx = .5*ggx.k(:)'*Cx*ggx.k(:);
+    neglogp = xneglogli+nlpt+nlpx;
     H = {Ht,Hx};  % Hessians for t and x components
 end
 
@@ -110,5 +120,4 @@ gg.dc = ggx.dc;
 gg.kt = ggt.kt;
 gg.kx = reshape(ggx.k',[],gg.krank);
 gg.k = (gg.ktbas*gg.kt)*gg.kx';
-fval = neglogli0; % value of loglikelihood
-
+neglogli = xneglogli;
